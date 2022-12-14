33b84a9e31218909b03c573fb8244c8c5b9015ea
SirLion02 <asadarifin123@gmail.com>
2022-12-14 09:44:24 +0700

    commit pertama

---
+++ auto.py
@@ -0,0 +1,37 @@
+import requests
+import time
+
+header = {
+    'authorization': 'MzU0NjQ2MjI1MjQ2NDg2NTI5.GtK2rg.1chCgx31ye0SAWQiOa9-iQTSNbqBjPwcgmDQso'
+    }
+
+header2 = {
+        'authorization' : 'OTU4NDM3NzA4MTYwNzI5MTE5.GSpwcT.ZcYQfM6cEQQkMBhIbcgzhu0y4Tv6ZSr-5ubz3Y'
+        }
+for x in range(10):
+    print(x)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/933286477473939486/messages" , json = {'content' : '!work'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/933286477473939486/messages" , json = {'content' : '!slut'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/933286477473939486/messages" , json = {'content' : '!crime'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/933286477473939486/messages" , json = {'content' : '!dep all'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/987796998105223188/messages" , json = {'content' : '$work'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/987796998105223188/messages" , json = {'content' : '$dep all'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/1043448069540298804/messages" , json = {'content' : 'wh'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/1043448069540298804/messages" , json = {'content' : 'wb'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/1043448069540298804/messages" , json = {'content' : 'owo'}, headers=header)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/826145279807455293/messages" , json = {'content' : 'wh'}, headers=header2)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/826145279807455293/messages" , json = {'content' : 'wb'}, headers=header2)
+    time.sleep(2)
+    r = requests.post("https://discord.com/api/v9/channels/826145279807455293/messages" , json = {'content' : 'owo'}, headers=header2)
+    time.sleep(6)
